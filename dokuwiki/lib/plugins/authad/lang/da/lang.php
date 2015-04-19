28d4f5553d81285c9383a8a353cac6576af53732
PavelHryzlik <hryzlik@gmail.com>
2015-04-19 04:32:28 +0200

    Contract json schema v1

--- dokuwiki/lib/plugins/authad/lang/da/lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Jacob Palm <mail@jacobpalm.dk>
- */
-$lang['domain']                = 'Logondomæne';
--- dokuwiki/lib/plugins/authad/lang/no/lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Patrick <spill.p@hotmail.com>
- */
-$lang['domain']                = 'Loggpå-domene';
